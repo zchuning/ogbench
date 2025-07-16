a1b2d501148a2fc7aaf7f8aa9cf40710350b5711
Chuning Zhu <chuning.zhu@gmail.com>
2025-07-16 02:42:27 +0000

    Update randmaze data collection script to collect standalone episodes (instead of one episode with multiple subepisodes). This ensures the data doesn't contain truncation.

--- data_gen_scripts/generate_randmaze.py
+++ data_gen_scripts/generate_randmaze.py
@@ -17,28 +17,19 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('seed', 0, 'Random seed.')
 flags.DEFINE_string('env_name', 'pointmaze-random-v0', 'Environment name.')
-flags.DEFINE_string('dataset_type', 'navigate', 'Dataset type.')
 flags.DEFINE_string('restore_path', 'experts/ant', 'Expert agent restore path.')
 flags.DEFINE_integer('restore_epoch', 400000, 'Expert agent restore epoch.')
 flags.DEFINE_string('save_path', None, 'Save path.')
 flags.DEFINE_float('noise', 0.2, 'Gaussian action noise level.')
-flags.DEFINE_integer('num_episodes', 1000, 'Number of episodes.')
-flags.DEFINE_integer('max_episode_steps', 1001, 'Maximum number of steps in an episode.')
+flags.DEFINE_integer('num_steps', 1000000, 'Number of steps.')
 
 
 def main(_):
-    assert FLAGS.dataset_type in ['path', 'navigate', 'stitch', 'explore']
-    # 'path': Reach a single goal and stay there.
-    # 'navigate': Repeatedly reach randomly sampled goals in a single episode.
-    # 'stitch': Reach a nearby goal that is 4 cells away and stay there.
-    # 'explore': Repeatedly follow random directions sampled every 10 steps.
-
     # Initialize environment.
     env = gymnasium.make(
         FLAGS.env_name,
         ob_type='pixels',
-        terminate_at_goal=False,
-        max_episode_steps=FLAGS.max_episode_steps,
+        max_episode_steps=100000, # Ensure never hit the max episode steps limit.
     )
 
     # Initialize oracle agent.
@@ -66,15 +57,14 @@ def main(_):
 
     # Collect data.
     dataset = defaultdict(list)
-    total_steps = 0
-    total_train_steps = 0
-    num_train_episodes = FLAGS.num_episodes
-    num_val_episodes = FLAGS.num_episodes // 10
-    for ep_idx in trange(num_train_episodes + num_val_episodes):
+    pbar = trange(FLAGS.num_steps, total=FLAGS.num_steps, desc='Collecting data')
+    step = 0
+    while step < FLAGS.num_steps:
+        # Sample a random maze
         maze_id = np.random.randint(len(env.unwrapped._maze_maps))
         maze_map = env.unwrapped._maze_maps[maze_id]
 
-        # Store all empty cells and vertex cells.
+        # Sample random initial and goal states.
         empty_cells = []
         vertex_cells = []
         for i in range(maze_map.shape[0]):
@@ -98,10 +88,9 @@ def main(_):
                     ):
                         continue
                     vertex_cells.append((i, j))
-    
-        # Sample an initial state from all cells.
+        # Sample initial state from all cells.
         init_ij = empty_cells[np.random.randint(len(empty_cells))]
-        # Sample a goal state from vertex cells.
+        # Sample goal state from vertex cells.
         valid_goals = [c for c in vertex_cells if c != init_ij]
         goal_ij = valid_goals[np.random.randint(len(valid_goals))]
     
@@ -109,8 +98,6 @@ def main(_):
         ob, _ = env.reset(options=dict(task_info=dict(maze_id=maze_id, init_ij=init_ij, goal_ij=goal_ij)))
 
         done = False
-        step = 0
-
         while not done:
             # Get the oracle subgoal and compute the direction.
             subgoal_xy, _ = env.unwrapped.get_oracle_subgoal(env.unwrapped.get_xy(), env.unwrapped.cur_goal_xy)
@@ -119,8 +106,7 @@ def main(_):
 
             # Query the oracle agent for the action.
             agent_ob = env.unwrapped.get_ob(ob_type='states')
-            # Exclude the agent's position and add the subgoal direction.
-            agent_ob = np.concatenate([agent_ob[2:], subgoal_dir])
+            agent_ob = np.concatenate([agent_ob[2:], subgoal_dir]) # exclude position and add subgoal direction.
             action = actor_fn(agent_ob, temperature=0)
             
             # Add Gaussian noise to the action.
@@ -128,37 +114,22 @@ def main(_):
             action = np.clip(action, -1, 1)
 
             next_ob, reward, terminated, truncated, info = env.step(action)
+            assert not truncated, 'Data collection should not trigger truncation.'
             done = terminated or truncated
-            success = info['success']
-
-            # Sample a new goal state when the current goal is reached.
-            if success and FLAGS.dataset_type == 'navigate':
-                goal_ij = vertex_cells[np.random.randint(len(vertex_cells))]
-                env.unwrapped.set_goal(goal_ij)
-
+            
             dataset['observations'].append(ob)
             dataset['actions'].append(action)
-            dataset['terminations'].append(terminated)
-            dataset["truncations"].append(truncated)
+            dataset['rewards'].append(reward)
+            dataset['dones'].append(done)
             dataset['qpos'].append(info['prev_qpos'])
             dataset['qvel'].append(info['prev_qvel'])
 
             ob = next_ob
             step += 1
+            pbar.update(1)
+    
+    print("Total number of steps: ", step)
 
-        total_steps += step
-        if ep_idx < num_train_episodes:
-            total_train_steps += step
-
-    print('Total steps:', total_steps)
-
-    train_path = FLAGS.save_path
-    val_path = FLAGS.save_path.replace('.npz', '-val.npz')
-    pathlib.Path(train_path).parent.mkdir(parents=True, exist_ok=True)
-
-    # Split the dataset into training and validation sets.
-    train_dataset = {}
-    val_dataset = {}
     for k, v in dataset.items():
         if 'observations' in k and v[0].dtype == np.uint8:
             dtype = np.uint8
@@ -166,11 +137,9 @@ def main(_):
             dtype = bool
         else:
             dtype = np.float32
-        train_dataset[k] = np.array(v[:total_train_steps], dtype=dtype)
-        val_dataset[k] = np.array(v[total_train_steps:], dtype=dtype)
-
-    for path, dataset in [(train_path, train_dataset), (val_path, val_dataset)]:
-        np.savez_compressed(path, **dataset)
+        dataset[k] = np.array(v, dtype=dtype)
+    pathlib.Path(FLAGS.save_path).parent.mkdir(parents=True, exist_ok=True)
+    np.savez_compressed(FLAGS.save_path, **dataset)
 
 
 if __name__ == '__main__':
